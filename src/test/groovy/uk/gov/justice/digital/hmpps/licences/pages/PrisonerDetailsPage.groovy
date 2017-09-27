9c7ebe18bbd0c92657b34b181d7c13c5ef34ac17
mattwhitfield <whitfield.mj@gmail.com>
2017-09-27 10:33:00 +0100

    added prisoner details tests

---
+++ src/test/groovy/uk/gov/justice/digital/hmpps/licences/pages/PrisonerDetailsPage.groovy
@@ -0,0 +1,24 @@
+package uk.gov.justice.digital.hmpps.licences.pages
+
+import geb.Page
+import uk.gov.justice.digital.hmpps.licences.modules.HeaderModule
+
+class PrisonerDetailsPage extends Page{
+
+    static url = '/details/A1235HG'
+
+    static at = {
+        browser.currentUrl.contains('/details/')
+    }
+
+    static content = {
+        header { module(HeaderModule) }
+
+        prisonerPersonalDetails { $('#personalDetails') }
+
+        prisonerKeyDates { $('#keyDates') }
+
+        continueBtns { $('#continueBtns') }
+    }
+
+}
