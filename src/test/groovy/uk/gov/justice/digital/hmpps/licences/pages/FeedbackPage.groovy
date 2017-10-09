37a159ae1dfe9217f061de0d9b7080b2d7c5d190
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2017-10-09 14:12:27 +0100

    Spec for feedback page

---
+++ src/test/groovy/uk/gov/justice/digital/hmpps/licences/pages/FeedbackPage.groovy
@@ -0,0 +1,22 @@
+package uk.gov.justice.digital.hmpps.licences.pages
+
+import geb.Page
+import uk.gov.justice.digital.hmpps.licences.modules.HeaderModule
+
+class FeedbackPage extends Page{
+
+    static url = '/feedback'
+
+    static at = {
+        browser.currentUrl.endsWith(url)
+    }
+
+    static content = {
+
+        header { module(HeaderModule) }
+
+        feedbackMailtoLink {  $('a', href: contains('mailto'))  }
+
+        backLink { $('a', text: contains('Back'))  }
+    }
+}
