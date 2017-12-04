3324fa2616ef4b9d6fa3daa088fba9763f79b112
Alistair Todd <alistair.todd@digital.justice.gov.uk>
2017-12-04 14:13:19 +0000

    Fixes for visible mode (ChromeDriver) when form elements need scroll

--- src/test/groovy/uk/gov/justice/digital/hmpps/licences/pages/AdditionalConditionsPage.groovy
+++ src/test/groovy/uk/gov/justice/digital/hmpps/licences/pages/AdditionalConditionsPage.groovy
@@ -1,6 +1,11 @@
 package uk.gov.justice.digital.hmpps.licences.pages
 
+import geb.Browser
 import geb.Page
+import org.openqa.selenium.By
+import org.openqa.selenium.JavascriptExecutor
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.interactions.Actions
 import uk.gov.justice.digital.hmpps.licences.modules.ButtonsModule
 import uk.gov.justice.digital.hmpps.licences.modules.HeaderModule
 
@@ -20,6 +25,14 @@ class AdditionalConditionsPage extends Page{
         noAdditionalConditions { $('a', text: 'No additional conditions required') }
 
         selectCondition { conditionId ->
+
+            WebElement element = driver.findElement(By.id('check-' + conditionId))
+
+            Browser.drive {
+                ((JavascriptExecutor) driver)
+                        .executeScript("arguments[0].scrollIntoView(true);", element)
+            }
+
             $('label', for: 'check-' + conditionId).click()
         }
     }
